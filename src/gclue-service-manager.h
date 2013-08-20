64d0858c63ad56c3f9b5f5da9e8b02aeb8fa6d8b
Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
2013-08-20 03:09:33 +0300

    service-manager: API to get number of connected clients

--- src/gclue-service-manager.h
+++ src/gclue-service-manager.h
@@ -54,8 +54,9 @@ struct _GClueServiceManagerClass
 
 GType gclue_service_manager_get_type (void) G_GNUC_CONST;
 
-GClueServiceManager * gclue_service_manager_new    (GDBusConnection *connection,
-                                                    GError         **error);
+GClueServiceManager * gclue_service_manager_new                   (GDBusConnection *connection,
+                                                                   GError         **error);
+guint                 gclue_service_manager_get_connected_clients (GClueServiceManager *manager);
 
 G_END_DECLS
 

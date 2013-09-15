a41d1c6970fcd506efcfc673ea7c88aa61720d5b
Zeeshan Ali (Khattak) <zeeshanak@gnome.org>
2013-09-15 05:21:36 +0300

    Generate documentation

--- autogen.sh
+++ autogen.sh
@@ -2,11 +2,15 @@
 
 set -e # exit on errors
 
+REQUIRED_AUTOMAKE_VERSION=1.9
+
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 mkdir -p m4
+PKG_NAME=GeoClue
 
 git submodule update --init --recursive
+gtkdocize
 autoreconf -v --force --install
 intltoolize -f
 
